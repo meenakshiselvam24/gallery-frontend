9164e3887289b2988f002c242e5627d7e147bde5
meenakshiselvam24 <meenakshiselvam16@gmail.com>
2024-12-16 21:00:39 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+
+ 
+  return (
+    <nav>
+      <div className="nav__logo">Photo Gallery App</div>
+     
+    </nav>
+  );
+};
+
+export default Navbar;
